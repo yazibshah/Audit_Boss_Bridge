b2fe5859d52b2842b583156fb20862ab585225f9
yazibshah <yazibshahnawaz123@gmail.com>
2025-11-18 23:16:01 +0500

    Initial commit

---
+++ src/L1Token.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.24;
+
+import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract L1Token is ERC20 {
+    uint256 private constant INITIAL_SUPPLY = 1_000_000;
+
+    // ok it's looks fine
+    constructor() ERC20("BossBridgeToken", "BBT") {
+        _mint(msg.sender, INITIAL_SUPPLY * 10 ** decimals());
+    }
+}
---
+++ src/L1Vault.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.24;
+
+import { IERC20 } from "@openzeppelin/contracts/interfaces/IERC20.sol";
+import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
+
+/// @title L1Vault
+/// @author Boss Bridge Peeps
+/// @notice This contract is responsible for locking & unlocking tokens on the L1 or L2
+/// @notice It will approve the bridge to move money in and out of this contract
+/// @notice It's owner should be the bridge
+contract L1Vault is Ownable {
+    IERC20 public token;
+
+    constructor(IERC20 _token) Ownable(msg.sender) {
+        token = _token;
+    }
+    // q why not just hard the approval to only the bridge?
+    function approveTo(address target, uint256 amount) external onlyOwner {
+        token.approve(target, amount);
+    }
+}
